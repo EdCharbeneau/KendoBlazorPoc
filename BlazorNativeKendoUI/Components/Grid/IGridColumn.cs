3c5f9fbfe5d7e7dfdffd812c14ca78cb22c8e402
Bozhidar Ivanchev <Bozhidar.Ivanchev@telerik.com>
2018-11-20 16:37:06 +0200

    feat: add basic columns

---
+++ BlazorNativeKendoUI/Components/Grid/IGridColumn.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorNativeKendoUI.Components.Grid
+{
+    public interface IGridColumn
+    {
+        string Field { get; set; }
+    }
+}
