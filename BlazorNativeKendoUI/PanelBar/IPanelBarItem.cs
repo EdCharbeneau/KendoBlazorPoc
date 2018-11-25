db1f94ad0cc19fba4f20176f5b387227235d4888
Ed Charbeneau <ed.charbeneau@gmail.com>
2018-11-25 00:46:08 -0500

    Basic panels working

---
+++ BlazorNativeKendoUI/PanelBar/IPanelBarItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorNativeKendoUI.PanelBar
+{
+    public interface IPanelBarItem
+    {
+        bool Selected { get; set; }
+        bool Expanded { get; set; }
+    }
+}
